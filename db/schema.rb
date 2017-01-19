17f8a52069b70f0ae51d606b7fafa804616dd853
Justin Tannenbaum <JustinEdwardTannenbaum@gmail.com>
2017-01-19 12:12:20 -0600

    add categories to jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170112171810) do
+ActiveRecord::Schema.define(version: 20170119000345) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170112171810) do
     t.string   "url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
 end
